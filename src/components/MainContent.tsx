4a395b2d45be9fbbc523063fa891b6e428d010c1
Cesar <cesar.alcoholado@hotmail.com>
2025-01-13 12:57:43 -0300

    added pagination

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -96,6 +96,34 @@ const MainContent = () => {
   const filteredProducts = getFilteredProducts()
 
   console.log(filteredProducts);
+
+  const totalProducts = 100
+  const totalPages = Math.ceil(totalProducts / itemsPerPage)
+
+  const handleCurrentPageChange = (page: number):void => {
+    if (page > 0 && page <= totalPages)
+    setCurrentPage(page)
+  }
+
+  const getPaginationButtons = ()=> {
+
+    const buttons: number[] = []
+    let startPage = Math.max(1, currentPage - 2);
+    let endPage = Math.min(totalPages, currentPage + 2)
+
+    if(currentPage - 2 < 1){
+      endPage = Math.min(totalPages, endPage + (2 - currentPage - 1));
+    }
+    if(currentPage + 2 > totalPages){
+      startPage = Math.min(totalPages, startPage - (2 - totalPages - currentPage));
+    }
+
+    for(let page = startPage; page <= endPage; page++) {
+      buttons.push(page)
+    }
+
+    return buttons
+  }
   
 
 
@@ -142,7 +170,22 @@ const MainContent = () => {
         {products.map((product, index)=> <BookCard key={index} id={product.id} title={product.title} image={product.thumbnail} price={product.price}/>)}
       </div>
 
+      <div className="flex flex-col sm:flex-row justify-between items-center mt-5">
+        <button className="border px-4 py-2 mx-2 rounded-full" onClick={()=> handleCurrentPageChange(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
+        <div className="flex flex-wrap justify-center">
+          {getPaginationButtons().map((page) => 
+            (<button key={page} onClick={()=> handleCurrentPageChange(page)} className={`border px-4 py-2 mx-1 rounded-full ${page === currentPage ? "bg-black text-white" : ""}`}>
+              {page}
+            </button>)
+          )}
+        </div>
+          <button className="border px-4 py-2 mx-2 rounded-full" onClick={()=> handleCurrentPageChange(currentPage + 1)} disabled={currentPage === totalPages}>Next</button>
+      </div>
+    {/* previous page
+    next page */}
     </div>
+
+    
     </section>;
 }
 
