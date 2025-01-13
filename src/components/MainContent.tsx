c2a118f8144077ef7356108ce0e87bb33a30abe9
Cesar <cesar.alcoholado@hotmail.com>
2025-01-13 10:41:27 -0300

    first commit

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,149 @@
+import { useEffect, useState } from "react";
+import { useFilter } from "../hooks/useFilters"
+import { Tally3 } from "lucide-react";
+import axios from "axios";
+import { BookCard } from "./BookCard";
+
+const MainContent = () => {
+
+  const { searchQuery, selectedCategory, minPrice, maxPrice, keyword } = useFilter()
+  
+  const [products, setProducts] = useState<Root>([]);
+  const [filter, setFilter] = useState<string>('all')
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [dropdownOpen, setDropdownOpen] = useState<boolean>(true)
+  const itemsPerPage = 12
+
+  type Root = Root2[];
+
+  interface Root2 {
+    id: number;
+    title: string;
+    description: string;
+    category: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    tags: string[];
+    brand: string;
+    sku: string;
+    weight: number;
+    dimensions: Dimensions;
+    warrantyInformation: string;
+    shippingInformation: string;
+    availabilityStatus: string;
+    reviews: Review[];
+    returnPolicy: string;
+    minimumOrderQuantity: number;
+    meta: Meta;
+    images: string[];
+    thumbnail: string;
+  }
+
+  interface Dimensions {
+    width: number;
+    height: number;
+    depth: number;
+  }
+
+  interface Review {
+    rating: number;
+    comment: string;
+    date: string;
+    reviewerName: string;
+    reviewerEmail: string;
+  }
+
+  interface Meta {
+    createdAt: string;
+    updatedAt: string;
+    barcode: string;
+    qrCode: string;
+  }
+
+  const getFilteredProducts = () => {
+    let filteredProducts = products;
+    
+    if (selectedCategory) {
+      filteredProducts = filteredProducts.filter((product) => product.category === selectedCategory)
+    }
+    
+    if (minPrice !== undefined) {
+      filteredProducts = filteredProducts.filter((product)=> product.price >= minPrice)
+    }
+
+    if (maxPrice !== undefined) {
+      filteredProducts = filteredProducts.filter((product)=> product.price <= maxPrice)
+    }
+
+    if (searchQuery !== undefined) {
+      filteredProducts = filteredProducts.filter((product)=> product.title.toLocaleLowerCase().includes(searchQuery.toLowerCase()))
+    }
+    
+    switch (filter) {
+      case 'expensive': 
+        return filteredProducts.sort((a,b)=> b.price - a.price);
+      case 'cheap':
+        return filteredProducts.sort((a, b) => a.price - b.price);
+      case 'popular':
+         return filteredProducts.sort((a, b) => a.rating - b.rating);
+      default: return filteredProducts
+    }
+    
+  }
+
+  const filteredProducts = getFilteredProducts()
+
+  console.log(filteredProducts);
+  
+
+
+  useEffect(() => {
+    let url = `https://dummyjson.com/products?limit=${itemsPerPage}&skip=${(currentPage - 1) * itemsPerPage}`;
+
+    if(keyword){
+      url = `https://dummyjson.com/products/search?q=${keyword}`;
+    }
+
+    const getData = async () => {
+      try {
+        const response: Root = await(await axios.get(url)).data.products;
+        setProducts(response)
+      } catch (error) {
+        console.error(error)
+      }
+      
+    }
+
+    getData()
+  }, [currentPage, keyword])
+  
+
+  return <section className="xl:w-[55rem] lg:w-[55rem] sm:w-[40rem] xs:w-[20rem] p-5">
+    <div className="mb-5">
+      <div className="flex flex-col sm:flex-row justify-between items-center">
+        <div className="relative mb-5 mt-5">
+          <button className="border px-4 py-2 rounded-full flex items-center">
+            <Tally3 className="mr-2"/>
+            {filter === 'all' ? 'Filter' : filter.charAt(0).toLowerCase() + filter.slice(1)}
+          </button>
+          {dropdownOpen && (
+            <div className="absolute bg-white border border-gray-300 rounded mt-2 w-full sm:w-40">
+              <button onClick={()=> setFilter('cheap')} className="block px-4 py-2 w-full text-left hover:bg-gray-200">Cheap</button>
+              <button onClick={()=> setFilter('expensive')} className="block px-4 py-2 w-full text-left hover:bg-gray-200">Expensive</button>
+              <button onClick={()=> setFilter('popular')} className="block px-4 py-2 w-full text-left hover:bg-gray-200">Popular</button>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="grid grid-cols-4 sm:grid-cols-3 md:grid-cols-4 gap-5">
+        {products.map((product, index)=> <BookCard key={index} id={product.id} title={product.title} image={product.thumbnail} price={product.price}/>)}
+      </div>
+
+    </div>
+    </section>;
+}
+
+export default MainContent
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,155 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+import { useFilter } from "../hooks/useFilters";
+
+interface Product {
+  category: string
+}
+
+interface Data {
+  products: Product[];
+}
+
+interface fetchResponse {
+    data: Data
+}
+
+const SideBar = () => {
+
+  const [categories, setCategories] = useState<string []>()
+  const [keywords] = useState<string[]>([
+    "apple",
+    "watch",
+    "fashion",
+    "trend",
+    "shoes",
+    "shirt",
+  ])
+
+  const {
+    searchQuery,
+    setSearchQuery,
+    selectedCategory,
+    setSelectedCategory,
+    minPrice,
+    setMinPrice,
+    maxPrice,
+    setMaxPrice,
+    keyword,
+    setKeyword,
+  } = useFilter();
+
+  const handleMinPriceChange = (e: React.ChangeEvent<HTMLInputElement>):void => {
+    const value = e.target.value;
+    setMinPrice(value ? parseFloat(value) : undefined);
+  };
+
+  const handleMaxPriceChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const value = e.target.value;
+    setMaxPrice(value ? parseFloat(value) : undefined)
+  }
+
+  const handleRadioChangeCategories = (category:string): void => {
+    setSelectedCategory(category)
+  }
+
+  const handleKeywordClick = (keyword:string): void=> {
+    setKeyword(keyword)
+  }
+
+  const handleResetFilters = (): void => {
+    setSearchQuery('')
+    setSelectedCategory('')
+    setMinPrice(undefined)
+    setMaxPrice(undefined)
+    setKeyword('')
+  }
+
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response: fetchResponse = await axios.get(
+          "https://dummyjson.com/products"
+        );
+        const uniqueCategories = Array.from(new Set(response.data.products.map((product) => product.category)))
+        setCategories(uniqueCategories)
+      } catch (error) {
+        console.error('Error fecthing caegories ', error)
+      }
+    }
+
+    fetchCategories()
+  }, [])
+  
+
+  return (
+    <div className="w-64 p-5 h-screen">
+      <h1 className="text-2xl font-bold mb-10 mt-4">React Store</h1>
+      <section>
+        <input
+          type="text"
+          className="border-2 rounded px-2 sm:mb-0"
+          placeholder="Search Product..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <div className="flex justify-center items-center">
+          <input
+            type="text"
+            className="border-2 mr-2 px-5 py-3 mb-3 w-full"
+            placeholder="Min"
+            value={minPrice ?? ''}
+            onChange={handleMinPriceChange}
+          />
+          <input
+            type="text"
+            className="border-2 mr-2 px-5 py-3 mb-3 w-full"
+            placeholder="Max"
+            value={maxPrice ?? ''}
+            onChange={handleMaxPriceChange}
+          />
+        </div>
+        {/*categories section*/}
+        <div className="mb-5">
+          <h2 className="text-xl font-semibold mb-3">Categories</h2>
+        </div>
+        <section>
+          {categories?.map((category, index) => (
+          <label className="block mb-2" key={index}>
+            
+                <input
+                  type="radio"
+                  name="category"
+                  value={category}
+                  className="mr-2 w-[16px] h-[16px]"
+                  onChange={()=> handleRadioChangeCategories(category)}
+                  checked={selectedCategory === category}
+                />
+                {category.toUpperCase()}
+              </label>
+          ))}
+        </section>
+        {/* {Keywords section} */}
+        <div className="mb-5 mt-4">
+          <h2 className="text-xl font-semibold mb-3">
+          keywords
+          </h2>
+          <div>
+            {keywords.map((keyword, index)=> 
+            <button key={index} className="block mb-2 px-4 py-2 w-full text-left border rounded hover:bg-gray-200"
+            onClick={()=> handleKeywordClick(keyword)}>
+              {keyword.toUpperCase()}
+            </button>)}
+          </div>
+        </div>
+        <button className="w-full mb-[4rem] py-2 bg-black text-white rounded mt-5"
+        onClick={handleResetFilters}>
+          Reset Filters
+        </button>
+      </section>
+    </div>
+  );
+}
+
+export default SideBar
